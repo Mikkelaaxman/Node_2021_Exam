14878cb989f4f95ae4fe188e177480e02b95c68d
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2021-02-15 11:22:58 +0100

    eslint test

--- 1._first_server/app.js
+++ 1._first_server/app.js
@@ -1,22 +1,29 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
+const cars = [
+  {
+    type: 'Volvo', topSpeed: 185, color: 'Black', id: 0,
+  },
+  {
+    type: 'BMW', topSpeed: 205, color: 'Blue', id: 1,
+  },
+  {
+    type: 'Tesla', topSpeed: 200, color: 'Red', id: 2,
+  },
+  {
+    type: 'Fiat', topSpeed: 165, color: 'White', id: 3,
+  },
+];
 
-let cars = [
-    {type: "Volvo", topSpeed: 185, color: "Black", id: 0},
-    {type: "BMW", topSpeed: 205, color: "Blue", id: 1},
-    {type: "Tesla", topSpeed: 200, color: "Red", id: 2},
-    {type: "Fiat", topSpeed: 165, color: "White", id: 3}
-]
-
-app.get("/cars", (req, res) => {
-    res.send(cars)
+app.get('/cars', (req, res) => {
+  res.send(cars);
 });
 
-app.get("/cars/:id", (req, res) => {
-    const car = cars.find(car => car.id === Number(req.params.id));
-    return res.send(car);
+app.get('/cars/:id', (req, res) => {
+  const carData = cars.find((carData) => carData.id === Number(req.params.id));
+  return res.send(carData);
 });
 
 app.listen(8080);
-
