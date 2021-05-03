41814c9a0e2cfd1a9324374a51cb7af48df59965
Mikkelaaxman <mikkelaaxman@gmail.com>
2021-05-03 13:33:25 +0200

    merging

--- 6._nodefolio/public/header/header.js
+++ 6._nodefolio/public/header/header.js
@@ -1,13 +1,17 @@
-$(document).jquery(function () {
+/* $(function () {
+    console.log("Header up");
     $(".nav li").removeClass("active");//this will remove the active class from  
     //previously active menu item 
     $('#contactLink').addClass('active');
     $('#skillsLink').addClass('active');
+    $('#projectLink').addClass('active');
 
-    $('#projectsLink').link(function () {
-        $('#projectLink').addClass('active');
+
+    $('#projectsLink').click( function () {
+        $('#projectsLink').addClass('active');
 
         $('.toast').toast({ animation: true, delay: 10000 });
         $('.toast').toast('show');
     });
 });
+ */
