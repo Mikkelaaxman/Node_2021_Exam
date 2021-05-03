4db8fe46132bf10f368c6cc4ddb8ec7ba7dba4e6
Mikkelaaxman <mikkelaaxman@gmail.com>
2021-05-03 14:51:32 +0200

    Fixed header path issue. Added clickable links to project.

--- 6._nodefolio/public/header/header.js
+++ 6._nodefolio/public/header/header.js
@@ -1,17 +1,8 @@
-/* $(function () {
-    console.log("Header up");
-    $(".nav li").removeClass("active");//this will remove the active class from  
-    //previously active menu item 
-    $('#contactLink').addClass('active');
-    $('#skillsLink').addClass('active');
-    $('#projectLink').addClass('active');
-
-
-    $('#projectsLink').click( function () {
-        $('#projectsLink').addClass('active');
-
-        $('.toast').toast({ animation: true, delay: 10000 });
-        $('.toast').toast('show');
+/* $(window).load(function () {
+    var checkvalue = window.location.pathname;
+    //alert(checkvalue);
+    $("a").each(function () {
+        if ($(this).attr('href') == checkvalue) { $(this).addClass("active"); }
     });
-});
- */
+
+}); */
