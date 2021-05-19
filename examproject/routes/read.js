8fa4f25ef401fb3e5b2450f6852951b25294dc85
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2021-05-19 14:10:58 +0200

    nodemailer

--- examproject/routes/read.js
+++ examproject/routes/read.js
@@ -6,22 +6,28 @@ const router = require("express").Router();
 const url = "mongodb://localhost:27017";
 const dbName = "beverages";
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        throw error;
-    }
+router.post("/api/read", (req, res) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+        if (error) {
+            throw error;
+        }
 
-    const db = client.db(dbName);
-    const wine = db.collection("wine");
+        const db = client.db(dbName);
+        const wine = db.collection("wine");
 
-    wine.find({ type: "white" }).toArray((error, data) => {
-        console.log(data);
-        client.close();
+        wine.find().toArray((error, foundWines) => {
+            if (error) {
+                throw error;
+            }
+            console.log(data);
+            client.close();
+            res.send({ data: foundWines })
+        });
     });
 });
 
-router.post("/api/read", (req, res) => {
-    console.log(req.body.email)
+router.get('/database/get', (req, res) => {
+    res.sendFile(`${__dirname}/public/see_wines.html`);
 });
 
 module.exports = {
