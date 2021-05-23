1775c74b51acf75c4f642b2c428cebb4e5fe2e76
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2021-05-23 23:12:17 +0200

    deleted unnessery files

--- examproject/routes/create.js
+++ examproject/routes/create.js
@@ -6,36 +6,32 @@ const router = require("express").Router();
 const url = "mongodb://localhost:27017";
 const dbName = "beverages";
 
-
-MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        throw new Error(error);
-    }
-
-    const db = client.db(dbName);
-    const wine = db.collection("wine");
-
-
-    let insert = {
-        type: req.body.type,
-        year: req.body.year,
-        name: req.body.name,
-        country: req.body.country
-    }
-
-    wine.insertOne({ insert }, (error, result) => {
+router.post("/api/createWine", (req, res) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
         if (error) {
             throw new Error(error);
         }
 
-        console.log(result);
-        client.close();
+        const db = client.db(dbName);
+        const wine = db.collection("wine");
+
+
+        let insert = {
+            type: req.body.type,
+            year: req.body.year,
+            name: req.body.name,
+            country: req.body.country
+        }
+
+        wine.insertOne({ insert }, (error, result) => {
+            if (error) {
+                throw new Error(error);
+            }
+
+            console.log(result);
+            client.close();
+        });
     });
-});
-
-
-router.post("/api/createWine", (req, res) => {
-    console.log(req.body.email)
 });
 
 module.exports = {
