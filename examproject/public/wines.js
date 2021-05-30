e9749953b6b95c7922fc114a9fa348b0e25e80b2
Mikkelaaxman <mikkelaaxman@gmail.com>
2021-05-30 19:04:54 +0200

    Fixed async issue in /all !

--- examproject/public/wines.js
+++ examproject/public/wines.js
@@ -1,10 +1,39 @@
-const { router } = require("../routes/read");
 
+/* let dataSet = [
+    { "_id": "60b2b12fcc9f7f4518a97592", "type": "Red", "year": "2019", "name": "Hardcoded Zin", "country": "USA, California" }
+];
+ */
+//doesnt work 
+async function getWines() {
+    const response = await fetch("/api/see_wine")
+
+/*         .then(result => result.json())
+        .then(json => {
+
+            for (let i = 0; i < Object.keys(json.foundWines).length; i++) {
+                dataSet.push(json.foundWines[i]);
+                console.log("Inside async "+ dataSet[i]);
+            }
+            return dataSet;
+        }) */
+        if(!response.ok){
+            throw new Error("An error has occured: " + response.text)
+        }
+    const dataSet = await response.json();
+    return dataSet;
+
+}
+
+
+function editWine(wine) {
+
+    console.log("EDIT WINE CALLED WITH " + wine._id)
+}
 
 $(document).ready(function () {
   
-    getWines();
-    console.log(dataSet[0]);
+    
+    //console.log(dataSet[0]);
 
 /*    DOESNT WORK
      console.log("Now creating table beginning with " + dataSet[0].name)
@@ -18,33 +47,44 @@ $(document).ready(function () {
             { title: "country" },
         ]
     }); */
-
+   
     var table = document.getElementById("wineTable");
 
-    for (let i = 0; i < dataSet.length; i++) {
-        //console.log(dataSet[i]);
+    getWines().then(dataSet =>{
 
-        // Create an empty <tr> element and add it to the 1st position of the table:
-        var row = table.insertRow(i+1); //+1 because header is 0
+        dataSet;
+        console.log(dataSet.foundWines);
 
-        // Insert new cells (<td> elements) at the the "new" <tr> element:
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
+        for (let i = 0; i < dataSet.foundWines.length; i++) {
 
-        wine = dataSet[i];
-        // Add some text to the new cells:
-        cell1.innerHTML = wine.name;
-        cell2.innerHTML = wine.country;
-        cell3.innerHTML = wine.type;
-        cell4.innerHTML = wine.year;
+            // Create an empty <tr> element and add it to the 1st position of the table:
+            var row = table.insertRow(i + 1); //+1 because header is 0
 
-        cell5.innerHTML = '<button id="editBtn" type="button" class="btn  btn-primary">Edit</button>';
+            // Insert new cells (<td> elements) at the the "new" <tr> element:
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
 
-        btn = document.getElementById("editBtn");
-        btn.onclick = (function (wine) { return function () { editWine(wine); } })(wine);
+            wine = dataSet.foundWines[i];
+            // Add some text to the new cells:
+            cell1.innerHTML = wine.name;
+            cell2.innerHTML = wine.country;
+            cell3.innerHTML = wine.type;
+            cell4.innerHTML = wine.year;
+
+            cell5.innerHTML = '<button id="editBtn" type="button" class="btn  btn-primary">Edit</button>';
+
+            btn = document.getElementById("editBtn");
+            btn.onclick = (function (wine) { return function () { editWine(wine); } })(wine);
+    
+        }
+        }).catch(error => {
+        error.message;
+    });
+
+
 /*      Proper way to do it, but cant appendChild to cell 
         var btn = document.createElement('input');
         btn.type = "button";
@@ -53,33 +93,10 @@ $(document).ready(function () {
        // btn.onclick = (function (value) { return function () { editWine(dataset); } })(entry);
 
         cell5.appendChild(btn); */
-    }
+    
     
 });
 
-let dataSet = [
-    { "_id": "60b2b12fcc9f7f4518a97592", "type": "Red", "year": "2019", "name": "Øko rød", "country": "Danmark" }
-];
-
-//doesnt work 
-function getWines() {
-    fetch("/api/see_wine")
-        .then(result => result.json())
-        .then(json => {
-
-            for (let i = 0; i < Object.keys(json.foundWines).length; i++) {
-                dataSet.push(json.foundWines[i]);
-                console.log(dataSet[i]);
-            }
-
-        }).catch(error => (console.log(error)));
-}
-
-
-function editWine(wine){
-   
-    console.log("EDIT WINE CALLED WITH " + wine._id)
-}
 
 //for when we add like button 
 /* function operateFormatter(value, row, index) {
